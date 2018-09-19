5ba0ef410a7381d30cd4eb1e06fc98c95ceb9704
maxence <maxence.rouget@gmail.com>
2018-09-19 11:34:12 +0200

    1 fst commit

---
+++ src/lib.cpp
@@ -0,0 +1,9 @@
+
+#include "lib.h"
+
+using namespace std;
+
+
+int lib::somme(int nombre_1,int nombre_2){
+  return (nombre_1 + nombre_2);
+}
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "lib.h"
+//#include <stdlib.h>
+
+int main() {
+
+    std::cout<<"HELLO"<<std::endl;
+
+
+
+    return 0;
+  }
