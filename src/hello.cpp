41ffc2e868f9458de4dacf9e61ecce5911cb9a47
maxence <maxence.rouget@gmail.com>
2018-09-26 10:30:34 +0200

    commit librairie

---
+++ src/hello.cpp
@@ -0,0 +1,14 @@
+#include "hello.h"
+#include <iostream>
+using namespace std;
+
+
+void parole::hello(){
+  cout<<"hello"<<endl;
+}
+void parole::bonjour(){
+  cout<<"bonjour"<<endl;
+}
+void parole::cava(){
+  cout<<"ça va ?!"<<endl;
+}
---
+++ src/hello.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class parole{
+
+public:
+  void hello();
+  void bonjour();
+  void cava();
+};
